f5e089840a7971ffb592a7bff83a75215887bc44
OSova2023 <ovf2019@gmail.com>
2024-06-19 18:20:06 +0300

    добавлено сообщение с дб через api. Пополнение базы комментов

--- dev/models/comment.ts
+++ dev/models/comment.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from "mongoose";
 
 const CommentSchema = new Schema({
+  id: { type: Number, required: false },
   name: { type: String, required: true },
   text: { type: String, required: true },
 });
--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Comments = void 0;
 const mongoose_1 = require("mongoose");
 const CommentSchema = new mongoose_1.Schema({
+    id: { type: Number, required: false },
     name: { type: String, required: true },
     text: { type: String, required: true },
 });
